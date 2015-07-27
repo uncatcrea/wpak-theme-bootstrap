03f38531289b7d899f35e40fae2d9a71ec10a2ba
Mathieu Le Roi <mathleroi@gmail.com>
2015-07-27 17:11:16 +0200

    Comment posting and feedback display

--- bootstrap/js/comments.js
+++ bootstrap/js/comments.js
@@ -2,18 +2,47 @@ define( [ 'jquery', 'core/theme-app', 'core/modules/comments' ], function( $, Ap
 
 	$( '#app-content-wrapper' ).on( 'submit', '#comment-form', function( e ) {
 		e.preventDefault();
+		
+		var $submit_button = $( '#comment-form [type="submit"]' );
+		$submit_button.attr( 'disabled', 'disabled' ).text( 'Sending...' );
+		
 		var comment = {
-			content : $( '#comment-content' ).val()
+			content : $( '#comment-form [name="content"]' ).val(),
+			post : $( '#comment-form [name="post"]' ).val()
 		};
+		
 		Comments.postComment( 
 			comment,
 			function( data ) {
-				console.log( 'Fini', data );
+				//Comment created OK!
+				
+				//Reset submit button :
+				$submit_button.removeAttr( 'disabled' );
+				
+				//Empty comment edition :
+				$( '#comment-form [name="content"]' ).val('');
+				
+				//Re-display current view (comments view) to display the new comment in the list :
+				App.rerenderCurrentScreen();
+				
+				//Scroll top : 
+				window.scrollTo( 0, 0 );
+				
+				//Display a happiness message :
+				$( '#feedback' ).removeClass( 'error' ).html( 'Comment added successfully :)' ).slideDown();
 			},
 			function( error ) {
-				console.log( 'ERREUR', error );
+				//Reset submit button :
+				$submit_button.removeAttr( 'disabled' ).text( 'Submit comment' );
+				
+				//Scroll top : 
+				window.scrollTo( 0, 0 );
+				
+				//Nothing else here as we choosed to handle error messages via events :
+				//they're displayed in the feedback div using the following "theme-event-message" filter.
 			}
 		);
+
 	} );
 	
 	App.filter( 'theme-event-message', function( message, event_data ) {
@@ -23,6 +52,9 @@ define( [ 'jquery', 'core/theme-app', 'core/modules/comments' ], function( $, Ap
 			switch ( event_data.event ) {
 				case 'comment:content-empty':
 					message = "Comment content is empty!";
+					break;
+				case 'comment:already-said-that':
+					message = "Comment already posted.";
 					break;
 				default:
 					message = "Comment error :(";
